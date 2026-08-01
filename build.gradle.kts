622767d7a39077fdb2e9aaba4d8e8e0575d17874
ariindra94 <85341661+ariindra94@users.noreply.github.com>
2026-08-01 14:34:56 +0700

    Update Android and Kotlin plugin versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,6 @@ pluginManagement {
 }
 
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id("com.android.application") version "8.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
